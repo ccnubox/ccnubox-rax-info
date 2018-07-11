da39beff1909464d9fde997a2afb1f762ca64dcb
zxc0328 <120797887@qq.com>
2018-07-11 19:55:54 +0800

    feat: fix padding-top

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import {createElement, Component, render} from 'rax';
-import View from 'rax-view';
-import Text from 'rax-text';
-import ListView from 'rax-listview';
-import InfoService from './services';
+import { createElement, Component, render } from "rax";
+import View from "rax-view";
+import Text from "rax-text";
+import ListView from "rax-listview";
+import InfoService from "./services";
 const test = require("@weex-module/test");
 
 // 将 item 定义成组件
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -18,66 +17,59 @@ class App extends Component {
   }
 
   componentWillMount() {
-    // alert(window.location.href)
-    InfoService.getInfoList()
-      .then((data) => {
-        this.setState({data})
-        test.changeLoadingStatus(true)
-      })
+    InfoService.getInfoList().then(data => {
+      this.setState({ data });
+      test.changeLoadingStatus(true);
+    });
   }
 
   listItem = (item, index) => {
     return (
       <View style={styles.item}>
         <Text style={styles.textHead}>{item.apartment}</Text>
-        {item.phone.map((num) => {
-          return <Text style={styles.text}>电话：{num}</Text>
+        {item.phone.map(num => {
+          return <Text style={styles.text}>电话：{num}</Text>;
         })}
         <Text style={[styles.text, styles.paddingTop]}>地址：{item.place}</Text>
       </View>
     );
-  }
+  };
   render() {
     return (
       <View style={styles.container}>
-      <ListView
-        renderRow={this.listItem}
-        dataSource={this.state.data}
-      />
+        <ListView renderRow={this.listItem} dataSource={this.state.data} />
       </View>
     );
   }
-
-};
+}
 
 const styles = {
   container: {
     flex: 1,
-    backgroundColor: '#efeff4',
-    paddingTop: 144,
-    paddingBottom: 100,
+    backgroundColor: "#efeff4",
+    paddingTop: 24,
+    paddingBottom: 100
   },
   textHead: {
-    color: '#6767fa',
+    color: "#6767fa",
     fontSize: 32,
     paddingTop: 32,
-    paddingBottom: 17,
+    paddingBottom: 17
   },
   text: {
     fontSize: 24,
-    color: '#434343',
+    color: "#434343"
   },
   item: {
     height: 250,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     marginBottom: 18,
     paddingLeft: 60,
     paddingRight: 60
   },
   paddingTop: {
-    paddingTop: 27,
+    paddingTop: 27
   }
-
 };
 
 export default App;
